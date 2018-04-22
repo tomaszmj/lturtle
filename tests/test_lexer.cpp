9bb899b7f06778df0f6d5d483cddcd792f7823c5
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-04-22 20:05:40 +0200

    changed 'hello test' from tutorial to real lexer test

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -1,12 +1,24 @@
 #include "source.h"
 #include "lexer.h"
+#include <string>
+#include <sstream>
 #define BOOST_TEST_MODULE lexer
 #include <boost/test/unit_test.hpp>
 
-int add( int i, int j ) { return i+j; }
-
 BOOST_AUTO_TEST_CASE( my_test )
 {
-    // seven ways to detect and report the same error:
-    BOOST_CHECK( add( 2,2 ) == 4 );
+    const std::string input = R"(
+    = # dskf '][ #-3454 *-85+3  ]
+
+                              -123 # ]''\;
+            # fdg
+        ;
+    )";
+    std::istringstream istr(input);
+    Source src(istr);
+    Lexer lex(src);
+    BOOST_CHECK(lex.getNextToken() == Lexer::equals_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::int_number);
+    BOOST_CHECK(lex.getLastReadInt() == -123);
+    BOOST_CHECK(lex.getNextToken() == Lexer::semicolon_symbol);
 }

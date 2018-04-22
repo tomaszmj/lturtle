7ab55dffa152d3ed3988b789532c890f8e754ebb
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-04-23 00:52:04 +0200

    test all symbols/operators

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -5,7 +5,8 @@
 #define BOOST_TEST_MODULE lexer
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( my_test )
+
+BOOST_AUTO_TEST_CASE( get_next_token_random )
 {
     const std::string input = R"(
     = # dskf '][ #-3454 *-85+3  ]
@@ -13,6 +14,7 @@ BOOST_AUTO_TEST_CASE( my_test )
                               -123 # ]''\;
             # fdg
         ;
+            00100
     )";
     std::istringstream istr(input);
     Source src(istr);
@@ -21,4 +23,24 @@ BOOST_AUTO_TEST_CASE( my_test )
     BOOST_CHECK(lex.getNextToken() == Lexer::int_number);
     BOOST_CHECK(lex.getLastReadInt() == -123);
     BOOST_CHECK(lex.getNextToken() == Lexer::semicolon_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::error);
 }
+
+
+BOOST_AUTO_TEST_CASE( get_next_token_symbols_and_operator )
+{
+    const std::string input = "=+{ }  (  ) ;,->";
+    std::istringstream istr(input);
+    Source src(istr);
+    Lexer lex(src);
+    BOOST_CHECK(lex.getNextToken() == Lexer::equals_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::plus_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::l_curly_bracket_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::r_curly_bracket_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::l_round_bracket_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::r_round_bracket_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::semicolon_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::colon_symbol);
+    BOOST_CHECK(lex.getNextToken() == Lexer::production_operator);
+}
+

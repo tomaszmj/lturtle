df6a763f5daa2f99822a2c87ce6c76eae4b763f5
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-04-22 16:52:58 +0200

    trivial unit test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include "lexer.h"
+#include "source.h"
 #include <iostream>
+#include <sstream>
+
+const std::string input = R"(
+= # dskf '][ #-3454 *-85+3  ]
+
+                          123 # ]''\;
+        # fdg
+    ;
+)";
 
 int main(int argc, char **argv)
 {
+    std::istringstream istr(input);
+    Source src(istr);
+    Lexer lex(src);
+    if(lex.getNextToken() == Lexer::equals_symbol)
+        std::cout << "equals ok\n";
+    if(lex.getNextToken() == Lexer::int_number)
+        std::cout << "int number: " << lex.getLastReadInt() << "\n";
+    if(lex.getNextToken() == Lexer::semicolon_symbol)
+        std::cout << "semicolon ok\n";
     return 0;
 }

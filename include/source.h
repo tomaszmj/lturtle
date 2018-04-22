2a87a9918f8399d9b00777ad007f03b47c3b1032
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-04-22 06:02:18 +0200

    trivial source implementation

--- include/source.h
+++ include/source.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <istream>
+
+class Source
+{
+public:
+    Source(std::istream &is);
+    char getNextChar();
+    unsigned getLine() const;
+    unsigned getPosition() const;
+
+private:
+    std::istream &input;
+    unsigned line;
+    unsigned position;
+};
--- src/source.cpp
+++ src/source.cpp
@@ -1,1 +1,31 @@
 #include "source.h"
+
+
+Source::Source(std::istream &is)
+    : input(is), line(0), position(0)
+{}
+
+char Source::getNextChar()
+{
+    if(!input)
+        return -1;
+    char c = input.get();
+    if(c == '\n')
+    {
+        position = 0;
+        ++line;
+    }
+    else
+        ++position;
+    return c;
+}
+
+unsigned Source::getLine() const
+{
+    return line;
+}
+
+unsigned Source::getPosition() const
+{
+    return position;
+}

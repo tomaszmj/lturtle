30f4d4c38004330aa37b98b4f5e3cdba8e15e439
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-04-22 06:02:40 +0200

    removed hash test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include "lexer.h"
 #include <iostream>
-#include <algorithm>
 
 int main(int argc, char **argv)
 {
-    for(const Lexer::Keyword &kw : Lexer::keywordHashTable)
-    {
-        std::cout << "hash(" << kw.string.data() << ") = " << Lexer::getHash(kw.string) << "\n";
-    }
     return 0;
 }

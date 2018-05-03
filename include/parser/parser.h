dea6c451c3e9ee9d637c6c518feac6fef22cd400
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-05-03 02:46:10 +0200

    renamed t->currentToken, added functions accept and parseTurtleOperationArguments, chnaged types returned by some functions to more specific (instead of generic Statement)

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -14,19 +14,23 @@ public:
 
 private:
     Lexer &lexer;
-    std::unique_ptr<Token> t;
+    std::unique_ptr<Token> currentToken;
 
     void nextToken();
     void consume(Token::Symbol token_type);
+    std::unique_ptr<Token> accept(Token::Symbol token_type);
+
     std::unique_ptr<Statement> parseStatement();
-    std::unique_ptr<Statement> parseRedefinition();
-    std::unique_ptr<Statement> parseDefinition();
-    std::unique_ptr<Statement> parseLiteralExecution();
-    std::unique_ptr<Statement> parseTurtleOperationExecution();
-    std::unique_ptr<Statement> parseProduction();
-    std::unique_ptr<Statement> parseOperationOrEvaluation();
-    std::unique_ptr<Statement> parseOperation();
-    std::unique_ptr<Statement> parseEvaluation();
+    std::unique_ptr<Definition> parseRedefinition();
+    std::unique_ptr<Definition> parseDefinition();
+    std::unique_ptr<LiteralExecution> parseLiteralExecution();
+    std::unique_ptr<TurtleOperationExecution> parseTurtleOperationExecution();
+    std::unique_ptr<Production> parseProduction();
+    std::unique_ptr<Definition> parseOperationOrEvaluation();
+    std::unique_ptr<Operation> parseOperation();
+    std::unique_ptr<Evaluation> parseEvaluation();
     std::unique_ptr<LiteralString> parseLiteralString();
     std::unique_ptr<TurtleOperation> parseTurtleOperation();
+    std::unique_ptr<TurtleOperation> parseTurtleOperationArguments(
+            TurtleOperation::Type operation_type, const char *arg_types);
 };

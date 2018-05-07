d00ff3c9480d58e9eafe4ccbfee526bc71ddc3a3
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-05-08 00:20:00 +0200

    wrote draft of function CodeAnalyzer::interpret

--- src/semantics/interpreter.cpp
+++ src/semantics/interpreter.cpp
@@ -18,5 +18,23 @@ CodeAnalyzer::CodeAnalyzer(std::istream &input)
 
 std::vector<std::unique_ptr<TurtleOperation>> &&CodeAnalyzer::interpret(UtmostTurtleCoordinates &coords)
 {
-
+    std::unique_ptr<parser_namespace::Statement> statement;
+    while((statement = parser.parseStatement()) != nullptr)
+    {
+        switch(statement->getType())
+        {
+            case parser_namespace::Statement::operation:
+            case parser_namespace::Statement::production:
+            case parser_namespace::Statement::evaluation:
+                variableMap.define(*dynamic_cast<parser_namespace::Definition*>(statement.get()));
+                break;
+            case parser_namespace::Statement::literal_execution:
+                //todo
+                break;
+            case parser_namespace::Statement::turtle_operation_execution:
+                //todo
+                break;
+        }
+    }
+    return std::move(turtleOperationsToReturn);
 }

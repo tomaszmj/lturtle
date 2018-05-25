4b70def0bd4fdee0400a9d6ebfed785dbede3c56
tomaszmj <tomasz.m.j.nowak@gmail.com>
2018-05-25 22:02:37 +0200

    created file for semantics tests

---
+++ src/tests/test_semantics.cpp
@@ -0,0 +1,12 @@
+#include "interpreter.h"
+#include "exception.h"
+#include <string>
+#include <sstream>
+#define BOOST_TEST_MODULE semantics
+#include <boost/test/unit_test.hpp>
+
+using namespace semantics_namespace;
+
+//BOOST_AUTO_TEST_CASE()
+//{
+//}
